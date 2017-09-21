db1899858abcb3ec74cb500b1bbe7081d7d8cce6
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-21 15:03:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,7 +21,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0, 8)
+  if index.between?(0, 8) && !(position_taken?(board, index))
     return true
   else
     return false
